ef274a64e1d3e65e3feeec2dadd3aa9d59e32476
AITGHERBI Massinissa <maitgherbi@wyplay.com>
2026-08-05 17:28:29 +0200

    set API level 36 as peer Play Console requirement before Aug 31

--- wycdn-sampleapp/build.gradle.kts
+++ wycdn-sampleapp/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.wyplay.wycdn.sampleapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.wyplay.wycdn.sampleapp"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = project.findProperty("versionCode")?.toString()?.toInt() ?: 1
         versionName = project.findProperty("versionName")?.toString() ?: "1.0"
 
